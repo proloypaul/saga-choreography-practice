9f28339d711082c00ebd0fcf998f7486ecfe4191
proloypaul <proloyp23@gmail.com>
2025-11-30 11:24:20 +0600

    implement saga choreography pattern using kafka

---
+++ booking-service/src/main/java/com/javatechie/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.javatechie.repository;
+import com.javatechie.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    Booking findByBookingCode(String bookingId);
+}
